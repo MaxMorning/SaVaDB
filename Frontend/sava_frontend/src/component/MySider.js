5d7b88aa1333f674b312be46db9e1759c60d5c88
MaxMorning <hml0814@163.com>
2022-05-17 16:07:19 +0800

    [FRONTEND] 主页动态加载用户信息完成

--- Frontend/sava_frontend/src/component/MySider.js
+++ Frontend/sava_frontend/src/component/MySider.js
@@ -11,7 +11,8 @@ import {
     ConsoleSqlOutlined,
     LogoutOutlined,
     InfoCircleOutlined,
-    CloudServerOutlined
+    CloudServerOutlined,
+    LoginOutlined
 } from '@ant-design/icons';
 import { createFromIconfontCN } from '@ant-design/icons';
 
@@ -30,39 +31,73 @@ const MyIcon = createFromIconfontCN({
 
 const { Title } = Typography;
 
-const items = [
-    getItem('Home', 'home', <HomeOutlined />),
-    getItem('Subscribe', 'subscribe', <DesktopOutlined />, [
-        getItem('Regions', 'regions', <MyIcon type="icon-earth" />),
-        getItem('Lineages', 'sub_lineages', <BranchesOutlined />),
-    ]),
-    getItem('Variants', 'variants', <MyIcon type="icon-dna" />, [
-        getItem('Search', 'search', <SearchOutlined />),
-        getItem('Lineages', 'lineages', <BranchesOutlined />),
-        getItem('Compare', 'compare', <MyIcon type="icon-compare" />),
-    ]),
-    getItem('Data', 'data', <DatabaseOutlined />, [
-        getItem('Statistics', 'statistics', <LineChartOutlined />),
-        getItem('API', 'api', <ConsoleSqlOutlined />),
-        getItem('Data source', 'dataSource', <CloudServerOutlined />)
-    ]),
-    getItem('User', 'user', <UserOutlined />, [
-        getItem('Info', 'info', <InfoCircleOutlined />),
-        getItem('Log out', 'logout', <LogoutOutlined />)
-    ]),
-];
-
 export default class MySider extends React.Component {
-    state = {
-        collapsed: false,
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: false,
+            didLogin: props.didLogin};
+    }
 
-    onCollapse = (collapsed) => {
-        console.log(collapsed);
+    didLoginItems = [
+        getItem('Home', 'home', <HomeOutlined />),
+        getItem('Subscribe', 'subscribe', <DesktopOutlined />, [
+            getItem('Regions', 'regions', <MyIcon type="icon-earth" />),
+            getItem('Lineages', 'sub_lineages', <BranchesOutlined />),
+        ]),
+        getItem('Variants', 'variants', <MyIcon type="icon-dna" />, [
+            getItem('Search', 'search', <SearchOutlined />),
+            getItem('Lineages', 'lineages', <BranchesOutlined />),
+            getItem('Compare', 'compare', <MyIcon type="icon-compare" />),
+        ]),
+        getItem('Data', 'data', <DatabaseOutlined />, [
+            getItem('Statistics', 'statistics', <LineChartOutlined />),
+            getItem('API', 'api', <ConsoleSqlOutlined />),
+            getItem('Data source', 'dataSource', <CloudServerOutlined />)
+        ]),
+        getItem('User', 'user', <UserOutlined />, [
+            getItem('Info', 'info', <InfoCircleOutlined />),
+            getItem('Log out', 'logout', <LogoutOutlined />)
+        ]),
+    ];
+
+    notLoginItems = [
+        getItem('Home', 'home', <HomeOutlined />),
+        getItem('Subscribe', 'subscribe', <DesktopOutlined />, [
+            getItem('Regions', 'regions', <MyIcon type="icon-earth" />),
+            getItem('Lineages', 'sub_lineages', <BranchesOutlined />),
+        ]),
+        getItem('Variants', 'variants', <MyIcon type="icon-dna" />, [
+            getItem('Search', 'search', <SearchOutlined />),
+            getItem('Lineages', 'lineages', <BranchesOutlined />),
+            getItem('Compare', 'compare', <MyIcon type="icon-compare" />),
+        ]),
+        getItem('Data', 'data', <DatabaseOutlined />, [
+            getItem('Statistics', 'statistics', <LineChartOutlined />),
+            getItem('API', 'api', <ConsoleSqlOutlined />),
+            getItem('Data source', 'dataSource', <CloudServerOutlined />)
+        ]),
+        getItem('Log in', 'login', <LoginOutlined/>),
+    ];
+
+    onCollapse = (inCollapsed) => {
+        console.log(inCollapsed);
         this.setState({
-            collapsed,
+            collapsed: inCollapsed,
         });
     };
+
+
+    static getDerivedStateFromProps(new_props, my_state) {
+        console.log("Enter");
+        if (new_props.didLogin !== my_state.didLogin) {
+            console.log("Hit");
+            return {
+                didLogin: new_props.didLogin
+            }
+        }
+        return null;
+    }
 
     render() {
         const { collapsed } = this.state;
@@ -79,6 +114,20 @@ export default class MySider extends React.Component {
             userName = <Title level={4} style={{color: 'white', margin: 0}} ellipsis={true} className='userName'>{user}</Title>;
         }
 
+        var my_menu;
+        if (this.state.didLogin) {
+            my_menu = <Menu theme="dark" defaultSelectedKeys={['home']} mode="inline" items={this.didLoginItems}/>;
+        }
+        else {
+            my_menu = <Menu theme="dark" defaultSelectedKeys={['home']} mode="inline" items={this.notLoginItems}/>;
+        }
+
+        var temp_items = {};
+        for (var key in this.items) {
+            temp_items[key] = this.items[key];
+        }
+        this.items = temp_items;
+
         return (
             <Layout.Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
                 <div className="logo" style={{
@@ -89,7 +138,7 @@ export default class MySider extends React.Component {
                 }}>
                 {userName}
                 </div>
-                <Menu theme="dark" defaultSelectedKeys={['home']} mode="inline" items={items} />
+                {my_menu}
             </Layout.Sider>
         );
     }
--- Frontend/sava_frontend/src/utils/Requester.js
+++ Frontend/sava_frontend/src/utils/Requester.js
@@ -3,7 +3,7 @@
  */
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8080'
+axios.defaults.baseURL = 'http://100.80.102.3:8080'
 
 export default class Requester {
     static getAuthToken() {
@@ -16,14 +16,24 @@ export default class Requester {
     }
 
     static requestJSON(request, withAuth, onSuccessCallback, onErrorCallback) {
-        if (withAuth == true) {
-            var token = this.getAuthToken();
+        if (withAuth === true) {
+            request.headers = {};
+            var token = Requester.getAuthToken();
             if (token == null) {
                 onErrorCallback("NO_TOKEN_AVAILABLE");
             }
             request.headers['Authorization'] = token;
         }
 
-        axios(request).then(onSuccessCallback).catch(onErrorCallback);
+        axios(request).then(
+            (response) => {
+                if (response.status !== 200) {
+                    // 处理错误情况
+                    alert("The request get status : " + response.status);
+                }
+                else {
+                    onSuccessCallback(response)
+                }
+            }).catch(onErrorCallback);
     }
 }
